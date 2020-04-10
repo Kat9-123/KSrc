cb8012795dee9fe8d398bf8d847a4d369138b921
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2020-04-10 10:46:34 +0200

    Update KSrc.py

--- KSrc.py
+++ KSrc.py
@@ -3,23 +3,28 @@ print("Importing")
 
 from ctypes import windll # Import lib to make programme DPI Aware
 from PIL import ImageGrab # Main screenshot lib
-from pynput.mouse import Listener # Mouse input lib
+from pynput.mouse import Listener, Controller # Mouse input lib
 from datetime import datetime # Date lib
 from tkinter import filedialog # Import Filedialog from tkinter for dir
 from tkinter import Tk # Import TK from tkinter 
+import threading
 import os # Import os lib
 from infi.systray import SysTrayIcon # Import infi.systray for systray icon
 import pathlib # Pathlib to get path
+from time import sleep
 print("Done Importing")
 
 user32 = windll.user32 # Set user32 var
 user32.SetProcessDPIAware() # Make programme DPI Aware
 
 # Set some vars
-loc1x = -1
-loc1y = -1
-loc2x = -1
-loc2y = -1
+mouse = Controller()
+loc1x = 0
+loc1y = 0
+loc2x = 0
+loc2y = 0
+StartedSc = False
+
 
 with open("Dir_Save", "r") as f: # Open the Dir_Save file
 	temp = f.read() # Read file and set to temp
@@ -37,25 +42,27 @@ def on_click(x, y, button, pressed):
 	global loc1x, loc1y
 	global loc2x, loc2y
 	global dirname
+	global StartedSc
 
 	if str(button) == "Button.right" and pressed == True: # Checks if right mouse was pressed and stops screenshotting
 		print("Cancelled")
 		return False
 		
 	# Some bodged checksums
-	if str(button) == "Button.left" and pressed == True:
-		if loc1x == -1:
+	if str(button) == "Button.left":
+		StartedSc = pressed
+
+		if pressed == True:
 			loc1x = x
 			loc1y = y
-			print("Pos 1 set to:", loc1x, loc1y)
 		else:
 			loc2x = x
 			loc2y = y
 
+
 			# Stops screenshotting if area is 1x1 
 			if loc1x == loc2x or loc1y == loc2y:
-				return False
-			print("Pos 2 set to:", loc2x, loc2y)
+				return False 
 
 			# Very good code to make the vars 
 			if loc1x > loc2x:
@@ -74,25 +81,85 @@ def on_click(x, y, button, pressed):
 
 
 			print("Saving: " + str(x1), str(y1), str(x2), str(y2))
+			sleep(0.05)
 			im = ImageGrab.grab(bbox=(x1,y1,x2,y2)) # Makes screenshot
-			now = datetime.now().strftime("%Y-%m-%d %H-%M-%S") # Makes date thingy
+			now = datetime.now().strftime("%Y-%m-%d %H_%M_%S") # Makes date thingy
 			im.save(dirname + str(now) + ".png") # Saves screenshot
 			print("Image Saved to: " + dirname)
 			return False
 
 
 
-def Screenshot(systray):
+def Screenshot():
 	global loc1x, loc1y, loc2x, loc2y
 	# Set vars back original value
-	loc1x = -1
-	loc1y = -1
-	loc2x = -1
-	loc2y = -1
+
 	print("Screenshotting . . .")
 	with Listener(on_click=on_click) as listener: # Start listener
 	    listener.join()
 
+
+def ScreenshotCall(systray):
+	global StartedSc
+	t1 = threading.Thread(target=Screenshot)
+	t1.start()
+	UIUpdate()
+	print("Threads Done")
+
+
+
+def UIUpdate():
+	global mouse, StartedSc, was, anchorx, anchory
+	posx = 0
+	posy = 0
+	anchorx = 0
+	anchory = 0
+	was = False
+	root = Tk()
+	def MouseUpdate():
+		global posx, posy, mouse, StartedSc, was, anchorx, anchory
+		global xa, ya, xs, ys
+		if StartedSc == True:
+			if was == False:
+				root.attributes("-fullscreen", False)
+				root.attributes("-alpha", 0.2)
+				anchorx = mouse.position[0]
+				anchory = mouse.position[1]
+			posx = mouse.position[0]
+			posy = mouse.position[1]
+
+			if anchorx > posx:
+				xa = posx
+				xs = anchorx
+			else:
+				xa = anchorx
+				xs = posx
+
+			if anchory > posy:
+				ya = posy
+				ys = anchory
+			else:
+				ya = anchory
+				ys = posy
+			xs -= xa
+			ys -= ya 
+
+			root.geometry(str(xs) + "x" + str(ys) + "+" + str(xa) + "+" + str(ya))
+			was = True
+		elif was == True:
+			root.destroy()
+		root.after(1, MouseUpdate)
+
+
+	
+	root.attributes("-alpha", 0.03)
+	root.attributes("-fullscreen", True)
+	
+	
+	root.attributes('-topmost', True)
+	root.overrideredirect(True)
+	MouseUpdate()	
+	root.mainloop()
 
 def OpenFolder(systray):
 	global dirname 
@@ -100,13 +167,13 @@ def OpenFolder(systray):
 
 def AskDir(systray):
 	global dirname 
-	root = Tk() # Set tkinter root
-	root.withdraw() # IDK stackoverflow said some
-	root.iconbitmap('KSrcIcon.ico') # Set icon
+	Dir = Tk() # Set tkinter root
+	Dir.withdraw() # IDK stackoverflow said some
+	Dir.iconbitmap('KSrcIcon.ico') # Set icon
 	tempdir = filedialog.askdirectory(initialdir=dirname,  title='Please select a directory') # Do some stuff
 	if tempdir != "": # Check if nothing was done
 		dirname = tempdir # They did something? Brilliant!
-	dirname += "\\" # Add this cuz python don't like me
+		dirname += "\\" # Add this cuz python don't like me
 	print("Dir set to: " + dirname)
 	with open("Dir_Save", "w+") as f: # Set the dir back to save file
 		f.write(dirname)
@@ -114,6 +181,6 @@ def AskDir(systray):
 
 
 # I don't really understand this. Just copied it from stackoverflow
-menu_options = (("Screenshot", None, Screenshot), ("Open Folder", None, OpenFolder), ("Set Directory", None, AskDir)) # Some systray stuff idk
+menu_options = (("Screenshot", None, ScreenshotCall), ("Open Folder", None, OpenFolder), ("Set Directory", None, AskDir)) # Some systray stuff idk
 systray = SysTrayIcon("KSrcIcon.ico", "KSrc", menu_options) # Even more systray stuff
 systray.start() # Start them systray

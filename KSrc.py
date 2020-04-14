20ebdfd6e430f55772159138077f3e9bf02e51c8
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2020-04-14 11:25:43 +0200

    Update KSrc.py

--- KSrc.py
+++ KSrc.py
@@ -4,13 +4,14 @@ print("Importing")
 
 from ctypes import windll # Import lib to make programme DPI Aware
 from PIL import ImageGrab # Main screenshot lib
-from pynput.mouse import Controller # Mouse input lib
+from pynput.mouse import Controller # Mouse controller for position
 from datetime import datetime # Date lib
-from tkinter import Tk, filedialog # Import TK from tkinter and import Filedialog from tkinter for dir
-from os import startfile # Import os lib
+from tkinter import Tk, filedialog # Import TK from tkinter and import Filedialog for dir
+from os import startfile, path # Import os lib
 from infi.systray import SysTrayIcon # Import infi.systray for systray icon
-from pathlib import Path# Pathlib to get path
-from win32api import GetKeyState
+from pathlib import Path # Pathlib to get path
+from win32api import GetKeyState # Mous input
+from sys import exit # For exit
 print("Done Importing")
 
 user32 = windll.user32 # Set user32 var
@@ -59,9 +60,13 @@ def Screenshot(systray):
 				print("Saving: " + str(xa), str(ya), str(xs), str(ys))
 				im = ImageGrab.grab(bbox=(xa,ya,xs,ys)) # Makes screenshot
 				now = datetime.now().strftime("%Y-%m-%d %H_%M_%S") # Makes date thingy
-				im.save(dirname + str(now) + ".png") # Saves screenshot
+				newfile = dirname + str(now) + ".png"
+
+				# Possible check if file already exists but I'm to lazy
+
+				im.save(newfile) # Saves screenshot
 				print("Image Saved to: " + dirname)
-				im.show()
+				startfile(newfile) # Opens file for user
 				return
 
 		if Pressed == True:
@@ -87,14 +92,14 @@ def Screenshot(systray):
 	root = Tk()
 
 
-	root.attributes("-alpha", 0.25)
-	root.attributes("-fullscreen", True)
-	root.configure(bg="grey")
+	root.attributes("-alpha", 0.25) # Set alpha of UI to 25%
+	root.attributes("-fullscreen", True) # Set UI to fullscreen
+	root.configure(bg="grey") # UI Background set to grey
 	
-	root.attributes('-topmost', True)
-	root.overrideredirect(True)
-	MainUpdate()	
-	root.mainloop()
+	root.attributes('-topmost', True) # Make the UI overlay
+	root.overrideredirect(True) # No topbar (This will disable your window to be closed by regular means)
+	MainUpdate()  # Start MainUpdate
+	root.mainloop() # Start Mainloop
 
 def OpenFolder(systray):
 	global dirname 
@@ -119,6 +124,8 @@ def AskDir(systray):
 
 def on_exit(systray):
 	exit()
+
+	
 
 menu_options = (("Screenshot", None, Screenshot), ("Open Folder", None, OpenFolder), ("Set Directory", None, AskDir)) # Some systray stuff idk
 systray = SysTrayIcon("KSrcIcon.ico", "KSrc", menu_options, on_quit=on_exit) # Even more systray stuff
